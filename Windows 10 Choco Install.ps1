b6f89440f538ebe00a7dff942d1419edab6b8a4e
Jake Brown <syth3sec@gmail.com>
2021-08-10 04:33:16 -0400

    Update Windows 10 Choco Install.ps1

--- Windows 10 Choco Install.ps1
+++ Windows 10 Choco Install.ps1
@@ -9,11 +9,13 @@ choco install iperf3 -y
 choco install nmap -y
 choco install notepadplusplus -y
 choco install postman -y
+choco install putty -y
 choco install pycharm-community -y
 choco install python -y
 choco install rsat -y
 choco install sysinternals -y
 choco install treesizefree -y
+choco install vagrant -y
 choco install vlc -y
 choco install vscode -y
 choco install microsoft-windows-terminal
@@ -22,22 +24,37 @@ choco install winscp -y
 choco install wireshark -y
 
 # Non-VM Tools
+choco install autohotkey -y
+choco install bitwarden -y
+choco install cinebench -y
+choco install cpu-z -y
 choco install discord -y
 choco install docker-desktop -y
+# NO EASY WINDOW SWITCHER INSTALLER
+choco install encrypto -y
+choco install everything -y
 choco install google-backup-and-sync -y
+choco install gpu-z -y
 choco install greenshot -y
+choco install hwinfo -y
+choco install hwmonitor -y
 choco install idrive -y
 choco install keepass -y
+choco install licecap -y
 choco install microsoft-office-deployment -y
 choco install obs-studio -y
 # NO PANDORA INSTALLER
 choco install powertoys -y
+choco install prime95.portable -y
 choco install rambox -y
+choco install rufus -y
+# NO SEARCH DEFLECTOR INSTALLER
 choco install spotify -y
 choco install steam-client -y
 choco install teamviewer -y
 choco install virtualbox -y
 choco install vmwareworkstation -y
+choco install winaero-tweaker
 choco install wsl -y
 choco install wsl-kalilinux -y
 choco install wsl-ubuntu-2004 -y
