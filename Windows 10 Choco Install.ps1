c0c9888aed12a0acc9080a7a85bfbef977a60c0c
Jake Brown <syth3sec@gmail.com>
2021-08-10 02:40:26 -0400

    Create Windows 10 Choco Install.ps1

---
+++ Windows 10 Choco Install.ps1
@@ -0,0 +1,40 @@
+# VM Tools
+choco install 7zip -y
+choco install burp-suite-free-edition -y
+choco install googlechrome -y
+choco install firefox -y
+choco install git -y
+choco install intellijidea-community -y
+choco install iperf3 -y
+choco install nmap -y
+choco install notepadplusplus -y
+choco install postman -y
+choco install pycharm-community -y
+choco install python -y
+choco install rsat -y
+choco install treesizefree -y
+choco install microsoft-windows-terminal
+choco install winmerge -y
+choco install winscp -y
+choco install wireshark -y
+choco install vlc -y
+choco install vscode -y
+
+# Non-VM Tools
+choco install discord -y
+choco install docker-desktop -y
+choco install google-backup-and-sync -y
+choco install greenshot -y
+choco install idrive -y
+choco install keepass -y
+choco install microsoft-office-deployment -y
+choco install obs-studio -y
+# NO PANDORA INSTALLER
+choco install powertoys -y
+choco install rambox -y
+choco install spotify -y
+choco install steam-client -y
+choco install teamviewer -y
+choco install virtualbox -y
+choco install vmwareworkstation -y
+choco install zoom -y
